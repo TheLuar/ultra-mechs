ee19beae9c797e0ba1e0b4734ad2790f933bbcdc
TheLuar <58670652+TheLuar@users.noreply.github.com>
2020-06-01 20:45:39 -0300

    asdasad

---
+++ clientjs/client.js
@@ -0,0 +1,1 @@
+console.log('lol it works')
--- index.js
+++ index.js
@@ -14,13 +14,18 @@ const keys = require('./whitelist.json')
 const game = 'data:application/x-shockwave-flash;base64,' + fs.readFileSync('./client.swf').toString('base64')
 
 
-// console.log(keys)
-
 server.listen(process.env.PORT || 3000)
 
 app.get('/', (req, res) =>
 {
 	const { k } = req.query
 	if (!keys.hasOwnProperty(k)) return res.status(400).send('👁️')
-	res.send('Accessing as ' + keys[k])
+	res.send(game)
 })
+
+app.get('/script', (req, res) =>
+{
+	res.send('./clientjs/client.js', { root: __dirname })
+})
+
+console.log(keys)
