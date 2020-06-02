6da26397b279b9c8817715aa444d785f84545b7b
TheLuar <58670652+TheLuar@users.noreply.github.com>
2020-06-01 21:20:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 
 
 const keys = require('./whitelist.json')
-const game = 'data:application/x-shockwave-flash;base64,' + fs.readFileSync('./client.swf').toString('base64')
+const game = 'data:application/x-shockwave-flash;base64,' + fs.readFileSync('./client/client.swf').toString('base64')
 
 
 server.listen(process.env.PORT || 3000)
@@ -22,7 +22,7 @@ app.get('/game', (req, res) =>
 	// const { k } = req.query
 	// if (!keys.hasOwnProperty(k)) return res.status(400).send('👁️')
 	// res.send(game)
-	res.sendFile(path.join(__dirname + '/client/client.swf'));
+	res.sendFile(path.join(__dirname + '/client/client.swf'))
 })
 
 app.get('/script', (req, res) =>
