0db6776ea9e9325a2f8fb3853d33ed223f22d731
TheLuar <58670652+TheLuar@users.noreply.github.com>
2020-06-01 23:09:45 -0300

    n bb

--- client/client.js
+++ client/client.js
@@ -6,37 +6,12 @@ console.log('%cmemento mori', 'background:#000000;color:#DDDDDD')
 
 // general
 
-const src = 'https://ultra-mechs.herokuapp.com/game'
-const game = document.querySelector('#game')
+// const src = 'https://ultra-mechs.herokuapp.com/game?x=y'
+// const game = document.querySelector('#game')
 
 
-fetch(src)
-.then(response => response.blob())
-.then(blob => game.data = URL.createObjectURL(blob))
-.catch(console.error);
+// fetch(src)
+// .then(response => response.blob())
+// .then(blob => game.data = URL.createObjectURL(blob))
+// .catch(console.error);
 
-
-
-// function httpGet(theUrl)
-// {
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-//     xmlHttp.send( null );
-//     return xmlHttp.responseText;
-// }
-// // const dataURLtoFile = (dataurl, filename) =>
-// // {
-// // 	const arr = dataurl.split(',')
-// // 	const mime = arr[0].match(/:(.*?);/)[1]
-// // 	const bstr = atob(arr[1])
-// // 	let n = bstr.length
-// // 	const u8arr = new Uint8Array(n)
-		
-// // 	while (n--) u8arr[n] = bstr.charCodeAt(n)
-	
-// // 	return new File([u8arr], filename, { type: mime })
-// // }
-
-// // //Usage example:
-// // var file = dataURLtoFile('data:text/plain;base64,aGVsbG8gd29ybGQ=', 'hello.txt')
-// // console.log(file)
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-const cors = require('cors')
+// const cors = require('cors')
 
 
 const app = express()
@@ -13,17 +13,17 @@ const server = http.createServer(app)
 
 
 const keys = require('./whitelist.json')
-const game = 'data:application/x-shockwave-flash;base64,' + fs.readFileSync('./client/client.swf').toString('base64')
+// const game = 'data:application/x-shockwave-flash;base64,' + fs.readFileSync('./client/client.swf').toString('base64')
 
 
-app.use(cors())
+// app.use(cors())
 
 app.get('/game', (req, res) =>
 {
 	// const { k } = req.query
 	// if (!keys.hasOwnProperty(k)) return res.status(400).send('👁️')
 	// res.send(game)
-	res.sendFile(path.join(__dirname + '/client/client.swf'))
+	res.sendFile(path.join(__dirname + '/client/bmmClient.swf'))
 })
 
 app.get('/script', (req, res) =>
